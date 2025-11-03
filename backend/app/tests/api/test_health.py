94eaf5d7f3f28104a7857937b8009f81e7f05448
Mikołaj Dawczyk <mikolaj.dawczyk@gmail.com>
2025-11-03 23:15:53 +0100

    test: reformated test structure

--- backend/app/tests/api/test_health.py
+++ backend/app/tests/api/test_health.py
@@ -1,12 +1,4 @@
-import pytest
-from fastapi.testclient import TestClient
-from backend.app.main import app
-
-
-@pytest.fixture
-def client():
-    with TestClient(app) as c:
-        yield c
+from backend.app.tests.conftest import client
 
 
 def test_health_check(client):
