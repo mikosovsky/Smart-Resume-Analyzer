957217609ba60aaaf068d42f180b1189715e700e
Mikołaj Dawczyk <mikolaj.dawczyk@gmail.com>
2025-12-03 00:07:37 +0100

    feat: Implemented PDFExtractor
    PDFExtractor is concrete class of DocumentExtractor (Abstract class) it is responssible for extracting data from pdfs to string.

--- backend/app/services/extract.py
+++ backend/app/services/extract.py
@@ -1,1 +1,33 @@
 # PDF/DOCX → text; sectioning & chunking
+from abc import ABC, abstractmethod
+from fastapi import UploadFile
+from fastapi.concurrency import run_in_threadpool
+import fitz  # PyMuPDF
+
+
+class DocumentExtractor(ABC):
+    @abstractmethod
+    def is_type(self, file_name: str) -> bool:
+        pass
+
+    @abstractmethod
+    async def extract_text(self, upload_file: UploadFile) -> str:
+        pass
+
+
+class PDFExtractor(DocumentExtractor):
+    def is_type(self, file_name: str) -> bool:
+        return file_name.lower().endswith(".pdf")
+
+    async def extract_text(self, upload_file: UploadFile) -> str:
+        content = await upload_file.read()
+        text = await run_in_threadpool(self._sync_extract, content)
+        return text
+
+    def _sync_extract(self, bytes_data: bytes) -> str:
+        doc = fitz.open(stream=bytes_data, filetype="pdf")
+        try:
+            text = "".join(str(page.get_text()) for page in doc)
+        finally:
+            doc.close()
+        return text
