9162c3de1ac742b5d4bfccf073c37dd733f74984
Mikołaj Dawczyk <mikolaj.dawczyk@gmail.com>
2025-12-04 18:01:42 +0100

    feat: Implemented JobDescriptionSchema and SkillSchema in schemas.py

--- backend/app/models/schemas.py
+++ backend/app/models/schemas.py
@@ -20,15 +20,32 @@ class EducationSchema(BaseModel):
     field_of_study: str = Field(..., description="Field of study or major")
 
 
+class SkillSchema(BaseModel):
+    hard_skills: list[str] = Field(..., description="List of hard skills")
+    soft_skills: list[str] = Field(..., description="List of soft skills")
+
 class ResumeSchema(BaseModel):
     name: str = Field(..., description="Full name of the candidate")
     email: str = Field(..., description="Email address of the candidate")
     phone_number: str = Field(..., description="Phone number of the candidate")
-    hard_skills: list[str] = Field(..., description="List of hard skills")
-    soft_skills: list[str] = Field(..., description="List of soft skills")
+    skills: list[SkillSchema] = Field(..., description="List of skills")
     experience: list[ExperienceSchema] = Field(
         ..., description="List of work experiences"
     )
     education: list[EducationSchema] = Field(
         ..., description="List of educational qualifications"
     )
+
+
+
+
+class JobDescriptionSchema(BaseModel):
+    title: str = Field(..., description="Job title")
+    company: str = Field(..., description="Company offering the job")
+    experience: str = Field(..., description="Required years of experience or level")
+    description: str = Field(..., description="Detailed job description")
+    required_skills: list[SkillSchema] = Field(..., description="List of required skills")
+    nice_to_have_skills: list[SkillSchema] = Field(
+        ..., description="List of nice-to-have skills"
+    )
+    responsibilities: list[str] = Field(..., description="List of job responsibilities")
