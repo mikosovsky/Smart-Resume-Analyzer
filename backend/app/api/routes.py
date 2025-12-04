084d0b26906cf93de9ccc3d93fb7d2182920f320
Mikołaj Dawczyk <mikolaj.dawczyk@gmail.com>
2025-12-04 20:07:01 +0100

    feat: Implemented /job-description/upload to transform jd into json format

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,7 +1,8 @@
 # Upload, create-job, match endpoints
 from fastapi import APIRouter, File, UploadFile
-from backend.app.services.extract import PDFExtractor
+from backend.app.services.extract import PDFExtractor, TXTExtractor
 from backend.app.services.resume_parser import ResumeParser
+from backend.app.services.jd_parser import JobDescriptionParser
 
 router = APIRouter()
 
@@ -14,4 +15,15 @@ async def upload_resume(file: UploadFile = File(...)):
         resume_parser = ResumeParser()
         parsed_resume = resume_parser.parse(resume_content)
         return parsed_resume
+    return {"error": "Unsupported file type"}
+
+
+@router.post("/job-description/upload")
+async def upload_job_description(file: UploadFile = File(...)):
+    txt_extractor = TXTExtractor()
+    if txt_extractor.is_type(file.filename):
+        jd_content = await txt_extractor.extract_text(file)
+        jd_parser = JobDescriptionParser()
+        parsed_jd = jd_parser.parse(jd_content)
+        return parsed_jd
     return {"error": "Unsupported file type"}
